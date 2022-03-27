72e56ed035a6780778e4a66a3eb6f74cc72e88dc
Ezra Lazuardy <ezralucio@gmail.com>
2022-03-27 23:16:05 +0700

    Monkey patch fix

--- src/pages/auth/sign-up.tsx
+++ src/pages/auth/sign-up.tsx
@@ -1,1 +1,31 @@
-export default {};
+import AppLayout from '@/components/layout/AppLayout';
+import Table from '@/components/Table';
+import { ReportData } from '@/constants/report-data';
+import { Box, ScrollArea, Text } from '@mantine/core';
+import React from 'react';
+
+type ReportProps = {};
+
+const Report = ({}: ReportProps) => {
+  return (
+    <AppLayout>
+      <Box py={24} sx={{ display: 'flex', flexDirection: 'column', gap: 24 }}>
+        <Text
+          sx={(theme) => ({
+            fontSize: 24,
+            fontWeight: 700,
+            letterSpacing: theme.other.letterSpacing.trackingTight,
+          })}
+        >
+          Report
+        </Text>
+
+        <ScrollArea>
+          <Table data={ReportData} />
+        </ScrollArea>
+      </Box>
+    </AppLayout>
+  );
+};
+
+export default Report;
