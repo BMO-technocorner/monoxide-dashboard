9fe5bf4aff616d6c61ccf6a7bc9db47fdb16adcd
[KW] Muhammad Bhaska <bhaska@kodingworks.io>
2022-03-28 23:02:32 +0700

    feat: added lint-staged, commitlint, etc

--- src/pages/devices.tsx
+++ src/pages/devices.tsx
@@ -1,13 +1,13 @@
-import AppLayout from '@/components/layout/AppLayout';
-import { Box } from '@mantine/core';
-import React from 'react';
+import AppLayout from "@/components/layout/AppLayout";
+import { Box } from "@mantine/core";
+import React from "react";
 
 type DevicesProps = {};
 
 const Devices = ({}: DevicesProps) => {
   return (
-    <AppLayout title='Devices'>
-      <Box sx={{ display: 'flex', flexDirection: 'column', gap: 16 }}></Box>
+    <AppLayout title="Devices">
+      <Box sx={{ display: "flex", flexDirection: "column", gap: 16 }}>Test</Box>
     </AppLayout>
   );
 };
