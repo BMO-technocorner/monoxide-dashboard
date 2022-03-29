9c2a5612a0f3d8a43ecc2ffcb92b7d5c496638e4
[KW] Muhammad Bhaska <bhaska@kodingworks.io>
2022-03-29 11:49:34 +0700

    feat: split to single component

--- src/components/ReportModal.tsx
+++ src/components/ReportModal.tsx
@@ -1,6 +1,7 @@
-import { ReportModalType } from '@/pages/report';
-import { Box, Button, Group, Modal, Text } from '@mantine/core';
-import { Check, PhoneCalling, X } from 'tabler-icons-react';
+import { Box, Button, Group, Modal, Text } from "@mantine/core";
+import { Check, PhoneCalling, X } from "tabler-icons-react";
+
+export type ReportModalType = "contact" | "resolve" | "details" | null;
 
 type ReportModalProps = {
   opened: ReportModalType;
@@ -12,25 +13,25 @@ export default function ReportModal({ opened, handleOpen }: ReportModalProps) {
     <Modal
       opened={opened !== null}
       onClose={() => handleOpen(null)}
-      title={opened === 'contact' ? 'Call emergency contact' : 'Resolve report'}
+      title={opened === "contact" ? "Call emergency contact" : "Resolve report"}
       centered
     >
-      {opened === 'contact' ? (
+      {opened === "contact" ? (
         <Box>
-          <Group grow direction='column'>
+          <Group grow direction="column">
             <Button
-              color='red'
-              variant='light'
+              color="red"
+              variant="light"
               leftIcon={<PhoneCalling size={14} />}
-              size={'xs'}
+              size={"xs"}
             >
               Call the authorities
             </Button>
             <Button
-              color='red'
-              variant='light'
+              color="red"
+              variant="light"
               leftIcon={<PhoneCalling size={14} />}
-              size={'xs'}
+              size={"xs"}
             >
               Call the nearest emergency contact
             </Button>
@@ -40,13 +41,13 @@ export default function ReportModal({ opened, handleOpen }: ReportModalProps) {
         <Box>
           <Group grow>
             <Button
-              color='green'
-              variant='light'
+              color="green"
+              variant="light"
               leftIcon={<Check size={14} />}
             >
               Yes
             </Button>
-            <Button color='red' variant='light' leftIcon={<X size={14} />}>
+            <Button color="red" variant="light" leftIcon={<X size={14} />}>
               No
             </Button>
           </Group>
