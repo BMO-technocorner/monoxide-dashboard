603e634aac476ae9997360fbc5ff25ad7ff2dea1
Ezra Lazuardy <ezralucio@gmail.com>
2022-03-30 18:21:51 +0700

    fix: wrangler configuration

---
+++ server.js
@@ -0,0 +1,25 @@
+const { createServer } = require("http");
+const { parse } = require("url");
+const next = require("next");
+
+const dev = process.env.NODE_ENV !== "production";
+const hostname = "localhost";
+const port = 3000;
+
+const app = next({ dev, hostname, port });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  createServer(async (req, res) => {
+    try {
+      const parsedUrl = parse(req.url, true);
+      await handle(req, res, parsedUrl);
+    } catch (err) {
+      console.error("Error occurred handling", req.url, err);
+      res.statusCode = 500;
+      res.end("Internal server error");
+    }
+  }).listen(port, () => {
+    console.log(`> Ready on http://${hostname}:${port}`);
+  });
+});
