986be450ed0e48f809cedea5160da55fc9fc7141
Ezra Lazuardy <ezralucio@gmail.com>
2022-03-30 18:40:38 +0700

    fix: wrangler configuration

--- server.mjs
+++ server.mjs
@@ -1,6 +1,6 @@
-const { createServer } = require("http");
-const { parse } = require("url");
-const next = require("next");
+import { createServer } from "http";
+import { parse } from "url";
+import next from "next";
 
 const dev = process.env.NODE_ENV !== "production";
 const hostname = "localhost";
